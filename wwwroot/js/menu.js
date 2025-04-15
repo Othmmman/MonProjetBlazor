5846f63e2d3cf57859eaf70469765e0b02b91c5a
Othman <othmanharchi@gmail.com>
2025-04-15 15:22:04 +0200

    Initial commit of Blazor Web project

---
+++ wwwroot/js/menu.js
@@ -0,0 +1,13 @@
+﻿function toggleSubMenu(id) {
+    const submenu = document.getElementById(id);
+    const all = document.querySelectorAll('.submenu');
+    all.forEach(el => {
+        if (el !== submenu) el.classList.remove('show');
+    });
+    submenu.classList.toggle('show');
+}
+
+function closeAllSubMenus() {
+    const all = document.querySelectorAll('.submenu');
+    all.forEach(el => el.classList.remove('show'));
+}
